1859b47499d43dbc8fc43b3b45e8022dc91a0386
J27mendez <mendes_j27@hotmail.com>
2025-07-04 11:08:36 -0300

    feat: add Cors

---
+++ src/utils/corsOptionsResponse.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+export function handleCorsOptions() {
+  return NextResponse.json(
+    {},
+    {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+    }
+  );
+}
