c69ad9d1a401264c12d867814cfae1239ce1671e
J27mendez <mendes_j27@hotmail.com>
2025-07-18 09:35:07 -0300

    chore: add middleware

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const response = NextResponse.next();
+
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    return response;
+  }
+
+  return response;
+}
